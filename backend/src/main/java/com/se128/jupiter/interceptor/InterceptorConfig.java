e73aad460410345346eba971065fdb8adef0e25f
shenhliu <55398344+shenhliu@users.noreply.github.com>
2020-07-30 09:24:54 +0800

    Revert "fix"
    This reverts commit 3aa08e8cb5efe59ad49659e5b774527e1176e4be.

--- backend/src/main/java/com/se128/jupiter/interceptor/InterceptorConfig.java
+++ backend/src/main/java/com/se128/jupiter/interceptor/InterceptorConfig.java
@@ -40,6 +40,7 @@ public class InterceptorConfig implements WebMvcConfigurer {
                 .addPathPatterns("/deleteGoodsByGoodsId")
                 .addPathPatterns("/editGoods")
                 .addPathPatterns("/addAuction")
+                .addPathPatterns("/addGoods")
                 .addPathPatterns("/deleteAuctionByAuctionId")
                 .addPathPatterns("/updateAuction")
                 .addPathPatterns("/editAuction")
